8610ae4b182c0d1317d6462758516f714f456edf
Geoff Longman <geoff@healthwave.co>
2017-11-17 08:05:17 -0500

    moved to hw_admin

--- app/models/spree/address_decorator.rb
+++
@@ -1,5 +0,0 @@
-Spree::Address.class_eval do
-  def avatax_cache_key
-    attributes.map { |k,v | v.to_s unless %w(id created_at updated_at).include?(k)}.join('-')
-  end
-end
--- app/models/spree/avalara_entity_use_code.rb
+++
@@ -1,5 +0,0 @@
-module Spree
-  class AvalaraEntityUseCode < ActiveRecord::Base
-    has_many :users, class_name: Spree.user_class.to_s, foreign_key: :avalara_entity_use_code_id
-  end
-end
